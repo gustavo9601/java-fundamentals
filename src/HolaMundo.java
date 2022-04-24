5195b3f66c5ba587522c2ee79257f396fc7d6fc6
gustavo9601 <tavo9601@gmail.com>
2022-04-24 12:21:22 -0500

    init app basic hello world

---
+++ src/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        /*
+        * System // sistema
+        * out // salida
+        * println // pinta con ln
+        * */
+        System.out.println("Hello World");
+    }
+
+}
