a9e8de2f44f6269c3a095285e8122abe5e5e1055
gustavo9601 <tavo9601@gmail.com>
2022-05-16 23:50:36 -0500

    format code

--- src/catalogo_productos/ejercicio/app/EjemploCatalogo.java
+++ src/catalogo_productos/ejercicio/app/EjemploCatalogo.java
@@ -9,22 +9,22 @@ import java.util.List;
 public class EjemploCatalogo {
     public static void main(String[] args) {
         List<IProducto> productos = Arrays.asList(
-          new TCLcd(500,"LG", 27),
-          new Libro(90, new Date(), "Gus", "POOJava", "Gus col"),
-          new IPhone(900, "Apple", "V13", "Gold")
+                new TCLcd(500, "LG", 27),
+                new Libro(90, new Date(), "Gus", "POOJava", "Gus col"),
+                new IPhone(900, "Apple", "V13", "Gold")
         );
 
         EjemploCatalogo.imprimir(productos);
     }
 
-    public static void imprimir(List<IProducto> productos){
+    public static void imprimir(List<IProducto> productos) {
         productos.forEach(producto -> {
-            if(producto instanceof IElectronico){
-                System.out.println("Fabricante = " + ((Electronico) producto).getFabricante() );
+            if (producto instanceof IElectronico) {
+                System.out.println("Fabricante = " + ((Electronico) producto).getFabricante());
             }
 
-            if(producto instanceof ILibro){
-                System.out.println("Titulo = " + ((Libro) producto).getTitulo() );
+            if (producto instanceof ILibro) {
+                System.out.println("Titulo = " + ((Libro) producto).getTitulo());
             }
 
             System.out.println("producto.getPrecioVenta() = " + producto.getPrecioVenta());
