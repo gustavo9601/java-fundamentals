2888a4157b1244544238df305f4c8e70fa93d4ff
gustavo9601 <tavo9601@gmail.com>
2022-05-21 00:25:55 -0500

    ejemplo genericos en clases

--- src/bolsa_supermercado/app/EjemploBolsaSupermercado.java
+++ src/bolsa_supermercado/app/EjemploBolsaSupermercado.java
@@ -10,14 +10,16 @@ import java.util.List;
 public class EjemploBolsaSupermercado {
     public static void main(String[] args) {
         BolsaSupermercado<Fruta> frutaBolsaSupermercado = new BolsaSupermercado<>();
-        frutaBolsaSupermercado.addProducto(new Fruta("Banano", 1000F, "Amarillo", 900F))
+        frutaBolsaSupermercado
+                .addProducto(new Fruta("Banano", 1000F, "Amarillo", 900F))
                 .addProducto(new Fruta("Pera", 200F, "Verde", 500F))
                 .addProducto(new Fruta("Manzana", 25F, "Rojo", 50000F));
 
         EjemploBolsaSupermercado.imprimir(frutaBolsaSupermercado.getProductos());
 
         BolsaSupermercado<Lacteo> lacteoaBolsaSupermercado = new BolsaSupermercado<>();
-        lacteoaBolsaSupermercado.addProducto(new Lacteo("Descremanada", 1000, 900, 900))
+        lacteoaBolsaSupermercado
+                .addProducto(new Lacteo("Descremanada", 1000, 900, 900))
                 .addProducto(new Lacteo("Muy dulce", 5000, 1900, 9020));
 
         EjemploBolsaSupermercado.imprimir(lacteoaBolsaSupermercado.getProductos());
